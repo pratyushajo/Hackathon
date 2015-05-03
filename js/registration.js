c98d8789b8f2645174ab4ddeb5eb6f68143d2543
pratyushajo <pjoginipally@scu.edu>
2015-05-02 23:48:49 -0700

    Registration JS changes with validation

--- js/registration.js
+++ js/registration.js
@@ -1,25 +1,40 @@
 var Regis = {
 	uid: 0,
+	
+	
+	
 	submit: function() {
 		var org = $('#OrgID').val(),
 			email = $('#newUserEmailID').val(),
 			address = $('#newUseraddressID').val(),
 			zip = $('#newUserzipcodeID').val(),
 			tel = $('#newUsertelID').val();
-		$.ajax({
-			type:"POST",
-			data:{	
-					org_name: org,
-					address: address,
-					phone_number: tel,
-					email_id: email,
-					zipcode: zip
-				},
-			url: 'http://students.engr.scu.edu/~schandra/hope/insert_userreg.php',
-			dataType: 'json',
-			async: false
-		});
-		this.getUserId();
+		var isValid = this.validateForm(org, email, address, zip, tel);
+		if(isValid) {	
+			$.ajax({
+				type:"POST",
+				data:{	
+						org_name: org,
+						address: address,
+						phone_number: tel,
+						email_id: email,
+						zipcode: zip
+					},
+				url: 'http://students.engr.scu.edu/~schandra/hope/insert_userreg.php',
+				dataType: 'json',
+				async: false
+			});
+			this.getUserId();
+		}
+	},
+	
+	validateForm: function(o, e, a, z, t){
+	   isValid = true;
+	   if ( o === "" || e === "" || a === "" || z === "" || t === "") {
+	       alert("All fields must be filled out");
+	       return false;
+	   }
+	   return isValid;
 	},
 	
 	getUserId: function() {
