44cf36066498d47b84bdcbe2d655fa30db7490ff
SamyukthaJ <samyukthaj08@gmail.com>
2015-05-02 23:51:55 -0700

    JS for training search

---
+++ js/userTrainingSearch.js
@@ -0,0 +1,70 @@
+var Search = {
+	zipcode: 0,
+	
+	go_buttonclick: function() {
+		var self = this;
+		this.zipcode = $('#searchJob').val();
+		
+		$.ajax({
+			type: "GET",
+			url: 'http://students.engr.scu.edu/~schandra/hope/gettrgpost_all.php',
+			dataType: 'json',
+			async: false,
+			crossDomain : true,
+			success: function(data) {
+				
+				self.setResource(data);
+			}
+		});
+		
+	},
+	setResource: function(dataPassed) {
+		
+		
+		var html = '';
+		for(var j=0; j< dataPassed.length; j++)
+		{
+			var resource = dataPassed[j];
+			resourcezip=resource.zipcode;
+			alert(resourcezip);
+			alert(this.zipcode);
+			if(resourcezip==this.zipcode)
+			{
+				debugger;
+				html += [
+					'<li>',
+					'<p>' , 
+					resource.address,
+					'</br>',
+					resource.description,
+					'</br>',
+					resource.email_id,
+					'</br>',
+					resource.employer,
+					'</br>',
+					resource.end_date,
+					'</br>',
+					resource.job_id,
+					'</br>',
+					resource.phone_number,
+					'</br>',
+					resource.start_date,
+					'</br>',
+					resource.title,
+					'</br>',
+					resource.cost,
+					'</li>'].join('');						
+		      }
+				$("#list_trainings").html(html);
+					
+			}	
+		
+		}				
+	
+	
+	
+	}
+
+
+
+
